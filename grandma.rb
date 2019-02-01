4521c278193b74aba484264fed46eadda1c915ab
Akram Helil <akramhelil@gmail.com>
2019-02-01 04:23:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,10 +17,8 @@ def speak_to_grandma(phrase)
    "HUH?! SPEAK UP, SONNY!"
  elsif phrase = "I LOVE YOU GRANDMA!"
  "I LOVE YOU TOO PUMPKIN!"
- elsif phrase = 'WHAT DID YOU EAT TODAY?'
- "NO, NOT SINCE 1938!"
- elsif phrase = "WHAT?"
-  "NO, NOT SINCE 1938!"
+else 
+  
  end
   
 end
